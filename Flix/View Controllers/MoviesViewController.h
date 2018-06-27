b8ed7581e9666a7c8aeeecaf21286be0824c5b7a
Jessica Shu <jessicashu7@fb.com>
2018-06-27 16:41:06 -0700

    launch screen, app icon, reorganization of files

---
+++ Flix/View Controllers/MoviesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesViewController.h
+//  Flix
+//
+//  Created by Jessica Shu on 6/27/18.
+//  Copyright © 2018 Jessica Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesViewController : UIViewController
+
+@end
